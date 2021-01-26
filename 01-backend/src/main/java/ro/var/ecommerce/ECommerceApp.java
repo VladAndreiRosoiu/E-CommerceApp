96ab7ca5e89e105f4454ca1c9df414c68374291c
VladAndreiRosoiu <rosoiuandreivlad@gmail.com>
2021-01-26 18:21:54 +0200

    created spring boot backend and basic angular frontend

---
+++ 01-backend/src/main/java/ro/var/ecommerce/ECommerceApp.java
@@ -0,0 +1,13 @@
+package ro.var.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceApp.class, args);
+	}
+
+}
---
+++ 01-backend/src/test/java/ro/var/ecommerce/ECommerceAppTests.java
@@ -0,0 +1,13 @@
+package ro.var.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
