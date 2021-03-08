4d8ba3e01d52f8bcf0fcfdf821f25c68a6a986d3
VladAndreiRosoiu <rosoiuandreivlad@gmail.com>
2021-03-08 12:32:25 +0200

    fixed login functionality

--- 02-frontend/src/app/config/my-app-config.ts
+++ 02-frontend/src/app/config/my-app-config.ts
@@ -1,7 +1,7 @@
 export default {
     oidc: {
         clientId: '0oa987i6uzSEPNDMk5d6',
-        issuer: 'http://dev-37335870.okta.com/oauth2/default',
+        issuer: 'https://dev-37335870.okta.com/oauth2/default',
         redirectUri:'http://localhost:4200/login/callback',
         scopes: ['openid', 'profile', 'email']
     }
